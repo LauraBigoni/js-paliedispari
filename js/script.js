b49cb0ced8810de5e0f0a4b973bf6a518b874cd5
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-14 15:17:41 +0100

    creo il ciclo while per controllare la parola

--- js/script.js
+++ js/script.js
@@ -13,9 +13,24 @@ Usiamo il DOM per stampare e chiedere le informazioni all'utente!
 const displayElement = document.getElementById('display');
 
 // # Faccio la domanda
-let word = prompt('Inserisci una parola');
+let parola = prompt('Inserisci una parola');
 
 // # Creo delle variabili di appoggio
 let parolaInversa = '';
+let lettera = '';
 
 // # Utilizzo while per creare il controllo della parola
+
+let i = parola.length -1;
+
+while (i > 0) {
+    lettera = parola[i];
+    parolaInversa += lettera;
+    console.log(lettera);
+    i--;
+}
+
+// # Stampo in pagina
+// message = `la parola ${parola}`;
+// display.innerHTML = `${message} è palindroma`;
+// display.innerHTML = ` ${message} non è palindroma`;
