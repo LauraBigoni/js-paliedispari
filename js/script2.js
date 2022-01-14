28bf3702ec2f9fcf2c66bce784fb92ea7b048375
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-14 16:23:59 +0100

    esercizio 2 domando il numero e randomizzo per pc

--- js/script2.js
+++ js/script2.js
@@ -12,3 +12,13 @@ BONUS:
 Usiamo il DOM per stampare e chiedere le informazioni all'utente!
 */
 
+// # Recupero gli elementi in pagina
+const displayElement = document.getElementById('display');
+
+// # Faccio la domanda all'utente
+let numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'));
+console.log(numeroUtente);
+
+// # Genero numero random CPU
+let numeroCpu = Math.floor(Math.random() * 5) + 1;
+console.log(numeroCpu);
